72040d4bc5837082b1b85e7b8e82e4db1c1216d4
Kristopher Paulsen <kpaulsen@bluehost.com>
2018-05-20 16:00:55 -0600

    Testing

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export BUILD_DIR=$1
+export CACHE_DIR=$2
+export ENV_DIR=$3
---
+++ detect.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# bin/use <build-dir>
+
+if [ -f $1/project.clj ]; then
+  grep ":min-lein-version[[:space:]]\+\"2" $1/project.clj > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Buildpack (Leiningen 2)" && exit 0
+  else
+    echo "Buildpack" && exit 0
+  fi
+else
+  (>&2 echo "Could not find a 'project.clj' file! Please ensure it exists and is checked into Git.")
+  exit 1
+fi
