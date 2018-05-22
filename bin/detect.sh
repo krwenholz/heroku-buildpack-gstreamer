b56c8f5f2e4a02ec51d55eb1ae54ff96bced397f
Kristopher Paulsen <kpaulsen@bluehost.com>
2018-05-21 19:40:07 -0600

    Restructure to follow other buildpacks

--- bin/detect.sh
+++ bin/detect.sh
@@ -4,9 +4,9 @@
 if [ -f $1/project.clj ]; then
   grep ":min-lein-version[[:space:]]\+\"2" $1/project.clj > /dev/null
   if [ $? -eq 0 ]; then
-    echo "Buildpack (Leiningen 2)" && exit 0
+    echo "Clojure (Leiningen 2)" && exit 0
   else
-    echo "Buildpack" && exit 0
+    echo "Clojure" && exit 0
   fi
 else
   (>&2 echo "Could not find a 'project.clj' file! Please ensure it exists and is checked into Git.")
